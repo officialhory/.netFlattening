7d1a8b515890bfb1b60a6a059c3bea9a5997eded
officialhory <hory01@live.com>
2020-02-16 20:45:29 +0100

    FlatteningJson

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace reflection
 {
@@ -40,16 +41,46 @@ namespace reflection
                 }
             };
 
+
+
             var dictionary = new Dictionary<string, string>();
 
             PrintPropsToDictionary(obj, dictionary);
+
             Console.WriteLine("***********************************");
             foreach (var kvp in dictionary)
             {
                 Console.WriteLine($"{kvp.Key} : {kvp.Value}");
             }
 
+            Console.WriteLine("************************************");
+
+            var jsonString =
+                "{\"gyumolcs\":{\"alma\":{\"szin\":\"piros\",\"alak\":\"kerek\"},\"korte\":{\"szin\":\"zold\",\"alak\":\"korte\",\"darab\":2},\"szilva\":{\"szin\":\"lila\",\"alak\":\"ovalis\",\"foo\":\"bar\"}},\"zoldseg\":{\"paprika\":{\"szin\":\"sarga\"}}}";
+
+            var flatJson = FlatteningJson(jsonString);
+
+
+            foreach (var kvp in flatJson)
+            {
+                Console.WriteLine($"{kvp.Key} : {kvp.Value}");
+            }
+
+
             Console.Read();
+        }
+
+        public static Dictionary<string,string> FlatteningJson(string jsonString)
+        {
+            JObject jsonObject = JObject.Parse(jsonString);
+            IEnumerable<JToken> jTokens = jsonObject.Descendants().Where(p => p.Count() == 0);
+            Dictionary<string, string> results = jTokens.Aggregate(new Dictionary<string, string>(), (properties, jToken) =>
+            {
+                properties.Add(jToken.Path, jToken.ToString());
+                return properties;
+            });
+
+            return results;
         }
 
         public static void PrintPropsToDictionary(object obj, Dictionary<string, string> dic, string link = null)
@@ -64,10 +95,9 @@ namespace reflection
                     Console.WriteLine((link == null) ? $"{p.Name}" : $"{link}.{p.Name}");
                     PrintPropsToDictionary(p.GetValue(obj), dic, (link == null) ? $"{p.Name}" : $"{link}.{p.Name}");
                 }
-
             }
             else
-            { 
+            {
                 if (link == null) return;
                 dic.Add(link, obj.ToString());
                 Console.WriteLine($"{link}.{obj}");
