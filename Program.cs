255fd2c510ac21f8d66e5044cdceecc84c831583
officialhory <hory01@live.com>
2020-02-16 20:53:03 +0100

    rename project

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace reflection
+namespace Flattening
 {
     class Program
     {
